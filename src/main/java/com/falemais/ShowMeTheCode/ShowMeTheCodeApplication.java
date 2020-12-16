123b1b21225c4534d2df2b7001f3bc15e39c13a2
AugustoCorreia <augusto.correia@live.com>
2020-12-15 21:53:26 -0300

    Skeletton

---
+++ src/main/java/com/falemais/ShowMeTheCode/ShowMeTheCodeApplication.java
@@ -0,0 +1,13 @@
+package com.falemais.ShowMeTheCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMeTheCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowMeTheCodeApplication.class, args);
+	}
+
+}
