a59e193e978e1104f02188c1b8541b5e7e37eb6a
Augusto Correia <Augusto.lira@Capgemini.com>
2021-02-09 20:01:15 -0300

    swagger

--- src/main/java/com/falemais/ShowMeTheCode/controller/CalculateController.java
+++ src/main/java/com/falemais/ShowMeTheCode/controller/CalculateController.java
@@ -3,6 +3,9 @@ package com.falemais.ShowMeTheCode.controller;
 import com.falemais.ShowMeTheCode.dto.CalculateRequest;
 import com.falemais.ShowMeTheCode.dto.CalculateResponse;
 import com.falemais.ShowMeTheCode.service.ICalculateService;
+
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +16,12 @@ public class CalculateController {
     @Autowired
     private ICalculateService calculateService;
 
-    @GetMapping(path = "/calc")
+    @ApiOperation(value = "Retorna Resultado do Calculo")
+    @PostMapping(path = "/calc")
     public CalculateResponse Calc (@RequestBody CalculateRequest calculateRequest) throws Exception {
         return calculateService.calc(calculateRequest);
     }
-    @GetMapping(path = "/tst")
+    @GetMapping(path = "/on")
     public String tst(){
         return "ONLINE!";
     }
